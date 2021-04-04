9f59dc4e38ac7da7bc8f6313cc2162fcbf019990
Yury Alencar <yuryalencar19@gmail.com>
2021-04-04 01:56:57 -0300

    feat(project): added new project and configurate .gitignore

---
+++ src/org/unipampa/edu/br/Main.java
@@ -0,0 +1,8 @@
+package org.unipampa.edu.br;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("SmartCoffe - An inteligent coffe machine");
+    }
+}
