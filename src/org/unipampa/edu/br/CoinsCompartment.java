f0a2a56d3585a155e17747dcae85efbe7d503787
Yury Alencar <yuryalencar19@gmail.com>
2021-04-17 13:31:16 -0300

    refactor(compartments): coins compartiments refactored

--- src/org/unipampa/edu/br/CoinsCompartment.java
+++ src/org/unipampa/edu/br/CoinsCompartment.java
@@ -2,154 +2,138 @@ package org.unipampa.edu.br;
 
 public class CoinsCompartment extends Compartment {
 
-    private int amountFiveCents = 30;
-    private int amountTenCents = 30;
-    private int amountTwentyFiveCents = 30;
-    private int amountFiftyCents = 30;
-    private int amountOneBRL = 30;
+    private int amountFiveCents;
+    private int amountTenCents;
+    private int amountTwentyFiveCents;
+    private int amountFiftyCents;
+    private int amountOneBRL;
 
     public CoinsCompartment() {
         super(100, 80, 15);
+
+        this.amountFiveCents = 0;
+        this.amountTenCents = 0;
+        this.amountTwentyFiveCents = 0;
+        this.amountFiftyCents = 0;
+        this.amountOneBRL = 0;
     }
 
     /**
      * Takes a number of coins of a given type from the coins compartment.
      *
-     * @param  type   one of five types of coin: fiveCents, tenCents, twentyFiveCents, fiftyCents e oneBRL
-     * @param  amount the amount of coins taken
-     * @return        whether the action was successful
+     * @param type   one of five types of coin: fiveCents, tenCents, twentyFiveCents, fiftyCents e oneBRL
+     * @param amount the amount of coins taken
+     * @return whether the action was successful
      */
     @Override
     public boolean take(String type, int amount) {
 
-        switch (type) {
-            case "fiveCents":
-                if(this.amountFiveCents - amount >= 0)
-                    this.amountFiveCents -= amount;
-                else {
-                    System.out.println("Quantidade de moedas superior ao limite em take().");
-                    return false;
-                }
-                break;
-
-            case "tenCents":
-                if(this.amountTenCents - amount >= 0)
-                    this.amountTenCents -= amount;
-                else {
-                    System.out.println("Quantidade de moedas superior ao limite em take().");
-                    return false;
-                }
-                break;
-
-            case "twentyFiveCents":
-                if(this.amountTwentyFiveCents - amount >= 0)
-                    this.amountTwentyFiveCents -= amount;
-                else {
-                    System.out.println("Quantidade de moedas superior ao limite em take().");
-                    return false;
-                }
-                break;
-
-            case "fiftyCents":
-                if(this.amountFiftyCents - amount >= 0)
-                    this.amountFiftyCents -= amount;
-                else {
-                    System.out.println("Quantidade de moedas superior ao limite em take().");
-                    return false;
-                }
-                break;
-
-            case "oneBRL":
-                if(this.amountOneBRL - amount >= 0)
-                    this.amountOneBRL -= amount;
-                else {
-                    System.out.println("Quantidade de moedas superior ao limite em take().");
-                    return false;
-                }
-                break;
-
-            default:
-                System.out.println("Tipo de copo não reconhecido em take().");
-                return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Fills the coins compartment of a given coin type.
-     *
-     * @param  type   one of five types of coin: fiveCents, tenCents, twentyFiveCents, fiftyCents e oneBRL
-     * @param  amount the total amount of coins after filled
-     * @return        whether the action was successful
-     */
-    @Override
-    public boolean fill(String type, int amount) {
-
-        // Check if amount is grater than max coin value
-        if(amount > this.getMaxAmount()) {
-            System.out.println("Quantidade de moedas superior ao permitido em fill().");
+        if (amount < 0) {
             return false;
         }
 
         switch (type) {
             case "fiveCents":
-                if(amount <= this.amountFiveCents) {
-                    System.out.println("Quantidade de moedas inferior ao esperado em fill().");
-                    return false;
+                if (this.amountFiveCents - amount >= 0) {
+                    this.amountFiveCents -= amount;
+                    return true;
                 }
-                else
-                    this.amountFiveCents = amount;
                 break;
 
             case "tenCents":
-                if(amount <= this.amountTenCents) {
-                    System.out.println("Quantidade de moedas inferior ao esperado em fill().");
+                if (this.amountTenCents - amount >= 0) {
+                    this.amountTenCents -= amount;
                     return false;
                 }
-                else
-                    this.amountTenCents = amount;
                 break;
 
             case "twentyFiveCents":
-                if(amount <= this.amountTwentyFiveCents) {
-                    System.out.println("Quantidade de moedas inferior ao esperado em fill().");
+                if (this.amountTwentyFiveCents - amount >= 0) {
+                    this.amountTwentyFiveCents -= amount;
                     return false;
                 }
-                else
-                    this.amountTwentyFiveCents = amount;
                 break;
 
             case "fiftyCents":
-                if(amount <= this.amountFiftyCents) {
-                    System.out.println("Quantidade de moedas inferior ao esperado em fill().");
-                    return false;
+                if (this.amountFiftyCents - amount >= 0) {
+                    this.amountFiftyCents -= amount;
+                    return true;
                 }
-                else
-                    this.amountFiftyCents = amount;
                 break;
 
             case "oneBRL":
-                if(amount <= this.amountOneBRL) {
-                    System.out.println("Quantidade de moedas inferior ao esperado em fill().");
-                    return false;
+                if (this.amountOneBRL - amount >= 0) {
+                    this.amountOneBRL -= amount;
+                    return true;
                 }
-                else
-                    this.amountOneBRL = amount;
+                break;
+        }
+
+        return false;
+    }
+
+    /**
+     * Fills the coins compartment of a given coin type.
+     *
+     * @param type   one of five types of coin: fiveCents, tenCents, twentyFiveCents, fiftyCents e oneBRL
+     * @param amount the total amount of coins after filled
+     * @return whether the action was successful
+     */
+    @Override
+    public boolean fill(String type, int amount) {
+
+        if (amount < 0) {
+            return false;
+        }
+
+        switch (type) {
+            case "fiveCents":
+                if (this.amountFiveCents + amount > this.getMaxAmount())
+                    return false;
+
+                this.amountFiveCents += amount;
+                break;
+
+            case "tenCents":
+                if (this.amountTenCents + amount > this.getMaxAmount())
+                    return false;
+
+                this.amountTenCents += amount;
+                break;
+
+            case "twentyFiveCents":
+                if (this.amountTwentyFiveCents + amount > this.getMaxAmount())
+                    return false;
+
+                this.amountTwentyFiveCents += amount;
+                break;
+
+            case "fiftyCents":
+                if (this.amountFiftyCents + amount > this.getMaxAmount())
+                    return false;
+
+                this.amountFiftyCents += amount;
+                break;
+
+            case "oneBRL":
+                if (this.amountOneBRL + amount > this.getMaxAmount())
+                    return false;
+
+                this.amountOneBRL += amount;
                 break;
 
             default:
-                System.out.println("Tipo de moeda não reconhecido em fill().");
                 return false;
         }
+
         return true;
     }
 
     /**
      * Verifies the current amount of coins of a given type.
      *
-     * @param  type   one of five types of coin: fiveCents, tenCents, twentyFiveCents, fiftyCents e oneBRL
-     * @return        the amount of coins, or -1 if an error occurred
+     * @param type one of five types of coin: fiveCents, tenCents, twentyFiveCents, fiftyCents e oneBRL
+     * @return the amount of coins, or -1 if an error occurred
      */
     @Override
     public int verifyAmount(String type) {
@@ -171,7 +155,6 @@ public class CoinsCompartment extends Compartment {
                 return this.amountOneBRL;
 
             default:
-                System.out.println("Tipo de moeda não reconhecido em verifyAmount().");
                 return -1;
         }
 
