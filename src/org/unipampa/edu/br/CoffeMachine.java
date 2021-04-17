585fa01270120dbecf8ec6ae3b89f74c8800d5c4
DESKTOP-14L06C0\User <jonaschagas10@gmail.com>
2021-04-17 17:30:35 -0300

    Adicionando interface do projeto.

--- src/org/unipampa/edu/br/CoffeMachine.java
+++ src/org/unipampa/edu/br/CoffeMachine.java
@@ -1,15 +1,93 @@
 package org.unipampa.edu.br;
 
-import java.util.ArrayList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class CoffeMachine {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class CoffeMachine implements Initializable {
+
+    //labels da tela principal
+    @FXML
+    private Label labelStatusCoffe;
+
+    @FXML
+    private Label labelCashBack;
+
+    @FXML
+    private TextField textFieldCoin;
+
+    //label da tela de login
+    @FXML
+    private TextField textFieldEmail;
+
+    //labels da tela de manutenção
+    @FXML
+    private Label labelStatusMaintenance;
+
+    @FXML
+    private Label labelWater;
+
+    @FXML
+    private Label labelGrain;
+
+    @FXML
+    private Label labelMilk;
+
+    @FXML
+    private Label labelCinnamon;
+
+    @FXML
+    private Label labelChocolate;
+
+    @FXML
+    private Label labelSugar;
+
+    @FXML
+    private Label labelGlassP;
+
+    @FXML
+    private Label labelGlassM;
+
+    @FXML
+    private Label labelGlassG;
+
+    @FXML
+    private Label labelCoinFiveCents;
+
+    @FXML
+    private Label labelCoinTenCents;
+
+    @FXML
+    private Label labelCoinTwentyFiveCents;
+
+    @FXML
+    private Label labelCoinFiftyCents;
+
+    @FXML
+    private Label labelCoinOne;
+
+    private Stage dialogStage;
+
     private ArrayList<Recipe> recipes;
     private Compartment cupsCompartment;
     private Compartment coinsCompartment;
     private Compartment ingredientsCompartment;
     private EmailCentral emailCentral;
 
-    public CoffeMachine() {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
         recipes = new ArrayList();
         cupsCompartment = new CupsCompartment();
         coinsCompartment = new CoinsCompartment();
@@ -249,5 +327,71 @@ public class CoffeMachine {
             }
         }
         return valid;
+    }
+
+    //método que renderiza a tela de login
+    @FXML
+    public void loginMachineSettings() throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(CoffeMachine.class
+                .getResource("/fxml/login.fxml"));
+        AnchorPane page = (AnchorPane) loader.load();
+
+        dialogStage = new Stage();
+        CoffeMachine controller = loader.getController();
+        controller.setDialogStage(dialogStage);
+        // Criando um Estágio de Diálogo (Stage Dialog)
+
+        dialogStage.setTitle("SmartCoffe");
+        dialogStage.setResizable(false);
+        Scene scene = new Scene(page);
+        dialogStage.setScene(scene);
+        dialogStage.show();
+    }
+
+    //Método que renderiza a tela de configurações da cafeteira
+    @FXML
+    public void machineSettings() throws IOException{
+
+        if(textFieldEmail.getText() != null && !textFieldEmail.getText().isEmpty()){
+            if(emailCentral.getTechnicianEmail().equalsIgnoreCase(textFieldEmail.getText())){
+                //Fecha o dialog de login e abre a tela de configuracoes da maquina
+                dialogStage.close();
+
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(CoffeMachine.class
+                        .getResource("/fxml/maintenance.fxml"));
+                AnchorPane page = (AnchorPane) loader.load();
+
+                dialogStage = new Stage();
+                CoffeMachine controller = loader.getController();
+                controller.setDialogStage(dialogStage);
+                // Criando um Estágio de Diálogo (Stage Dialog)
+
+                dialogStage.setTitle("SmartCoffe - Configurações da Cafeteira");
+                dialogStage.setResizable(false);
+                Scene scene = new Scene(page);
+                dialogStage.setScene(scene);
+                dialogStage.showAndWait();
+            }
+            else{
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Erro na autenticação");
+                alert.setHeaderText(null);
+                alert.setContentText("Email incorreto");
+                alert.showAndWait();
+            }
+        }
+    }
+
+    //método que fecha a tela de configurações da máquina
+    @FXML
+    public void close() throws IOException{
+        this.dialogStage.close();
+    }
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
     }
 }
--- src/org/unipampa/edu/br/Main.java
+++ src/org/unipampa/edu/br/Main.java
@@ -1,11 +1,33 @@
 package org.unipampa.edu.br;
 
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.util.ArrayList;
 
-public class Main {
+public class Main extends Application {
+
+    private Stage stage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("SmartCoffe");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+        setStage(stage);
+    }
 
     public static void main(String[] args)  {
+
+        launch(args);
 
         System.out.println("SmartCoffee - An intelligent coffee machine");
 
@@ -66,5 +88,9 @@ public class Main {
         System.out.println("Coffee: " + ingr.verifyAmount("coffee"));
         System.out.println("Sugar: " + ingr.verifyAmount("sugar"));*/
 
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
     }
 }
